7a36c16440f8f186616dd957f16274f3022c6246
MyriadColors <pedrotorresgamedesign@gmail.com>
2023-10-09 04:42:11 -0300

    Desafio feito.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Criando celular nokia...");
+var nokia = new Nokia("92389183", "G60", "832179273987126387916", memoria: 512);
+nokia.Ligar("92213289183");
+nokia.ReceberLigacao("92213289183");
+
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Criando celular Iphone...");
+var iPhone = new Iphone("92213289183", "IPhone 12X", "3831892839", memoria: 1024);
+iPhone.Ligar("92389183");
+iPhone.ReceberLigacao("92389183");
+iPhone.InstalarAplicativo("Telegram");
